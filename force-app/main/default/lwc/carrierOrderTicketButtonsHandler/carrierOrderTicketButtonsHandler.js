c75eb57eca187adedbfcbc0d0626755590a219a7
Matt Higel <matt.higel@gusto.com>
2025-02-21 04:05:44 +0000

    CI deployment from job `UAT Sandbox to UAT Branch Sync`

--- force-app/main/default/lwc/carrierOrderTicketButtonsHandler/carrierOrderTicketButtonsHandler.js
+++ force-app/main/default/lwc/carrierOrderTicketButtonsHandler/carrierOrderTicketButtonsHandler.js
@@ -1,9 +1,31 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 import { NavigationMixin } from 'lightning/navigation';
 import { encodeDefaultFieldValues } from "lightning/pageReferenceUtils";
-export default class CarrierOrderTicketButtonsHandler extends NavigationMixin (LightningElement) {
+export default class CarrierOrderTicketButtonsHandler extends NavigationMixin(LightningElement) {
     _recordId;
     showRecord = false;
+    objectApiName;
+    isBO = false;
+    isCO = false;
+
+    BENEFIT_ORDER_OBJECT = 'Benefit_Order__c';
+    CARRIER_ORDER_OBJECT = 'Carrier_Order__c';
+    BENEFIT_ORDER_TICKET_URL = (recordId) => `/apex/CreateNewTicketPage?id=${recordId}&from=Ticket`;
+    BENEFIT_ORDER_QC_ERROR_URL = (recordId) => `/apex/CreateNewQCErrorBO?id=${recordId}&from=QCError`;
+    CARRIER_ORDER_TICKET_URL = (recordId) => `/apex/CreateNewTicketPageCO?id=${recordId}&from=Ticket`;
+    CARRIER_ORDER_QC_ERROR_URL = (recordId) => `/apex/CreateNewQCErrorCO?id=${recordId}&from=QCError`;
+
+    @wire(getRecord, { recordId: '$recordId', layoutTypes: ['Full'], modes: ['View'] })
+    wiredRecord({ error, data }) {
+        if (data) {
+            this.objectApiName = data.apiName;
+            this.isBO = this.objectApiName === this.BENEFIT_ORDER_OBJECT;
+            this.isCO = this.objectApiName === this.CARRIER_ORDER_OBJECT;
+        } else if (error) {
+            console.error('Error fetching record:', error);
+        }
+    }
 
     @api set recordId(value) {
         this._recordId = value;
@@ -14,24 +36,28 @@ export default class CarrierOrderTicketButtonsHandler extends NavigationMixin (L
         return this._recordId;
     }
 
-    newTicket(){
-        let sr  = '/apex/CreateNewTicketPageCO?id='+this._recordId+'&from=Ticket';
-        console.log('sr', sr);
+    newTicket() {
+        let newTicketURL = this.isBO
+            ? this.BENEFIT_ORDER_TICKET_URL(this._recordId)
+            : this.CARRIER_ORDER_TICKET_URL(this._recordId);
+
         this[NavigationMixin.Navigate]({
             type: 'standard__webPage',
             attributes: {
-                url: sr
+                url: newTicketURL
             }
         });
     }
 
-    newQCError(){
-        let sr  = '/apex/CreateNewQCErrorCO?id='+this._recordId+'&from=QCError';
-        console.log('sr', sr);
+    newQCError() {
+        let qcErrorURL = this.isBO
+            ? this.BENEFIT_ORDER_QC_ERROR_URL(this._recordId)
+            : this.CARRIER_ORDER_QC_ERROR_URL(this._recordId);
+
         this[NavigationMixin.Navigate]({
             type: 'standard__webPage',
             attributes: {
-                url: sr
+                url: qcErrorURL
             }
         });
     }
@@ -40,9 +66,9 @@ export default class CarrierOrderTicketButtonsHandler extends NavigationMixin (L
         const defaultValues = encodeDefaultFieldValues({
             WhatId: this._recordId
         });
-        
-        this[NavigationMixin.Navigate]( {
-           type: "standard__objectPage",
+
+        this[NavigationMixin.Navigate]({
+            type: "standard__objectPage",
             attributes: {
                 objectApiName: "Task",
                 actionName: "new",
@@ -53,5 +79,4 @@ export default class CarrierOrderTicketButtonsHandler extends NavigationMixin (L
             }
         });
     }
-
 }
